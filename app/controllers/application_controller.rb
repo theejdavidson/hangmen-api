2c63c329b9c4e6b3b1576b336b275945f565c096
Ethan Davidson <theejdavidson@gmail.com>
2020-05-12 17:21:13 -0500

    backend setup

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,36 @@
+class ApplicationController < ActionController::API
+    before_action :authorized
+    def encode_token(payload)
+        JWT.encode(payload, 'f00_b@r') #store secret in ENV var
+    end
+
+    def auth_header
+        request.headers['Authorization']
+    end
+
+    def decoded_token
+        if auth_header
+            token = auth_header.split(' ')[1]
+            begin
+                JWT.decode(token, 'f00_b@r', true)
+            rescue JWT::DecodeError
+                nil
+            end
+        end
+    end
+
+    def current_user
+        if decoded_token
+            user_id = decoded_token[0]['user_id']
+            @user = User.find_by(id: user_id)
+        end
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def authorized
+        render json: { message: 'Please log in'}, status: :unauthorized unless logged_in?
+    end
+end
