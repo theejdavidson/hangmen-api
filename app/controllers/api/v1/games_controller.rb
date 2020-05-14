45d44cd8257295a2fa2cedf4de085be276ecf68b
Ethan Davidson <theejdavidson@gmail.com>
2020-05-14 18:44:49 -0500

    added guessword route, serializer does not recognize update

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -12,10 +12,28 @@ class Api::V1::GamesController < ApplicationController
     end
 
     def join
-        @game = Game.find_by(key: params[:key])
+        @game = Game.find_by(key: params[:inviteKey])
         if @game
             @game_user = GameUser.create(game_id: @game.id, user_id: params[:user][:id])
             broadcast_game
+        else
+            render json: { error: 'could not find game with that key'}, status: :not_acceptable
+        end
+    end
+
+    def add_guess_word
+        @game = Game.find_by(key: params[:invite_key])
+        if @game
+            @game_user = GameUser.find_by(user_id: params[:user_id])
+            if @game_user
+                # byebug
+                @game_user.update(guess_word: params[:guess_word])
+                @game_user.save
+                byebug
+                broadcast_game
+            else
+                render json: { error: 'could not find game user'}, status: :not_acceptable
+            end
         else
             render json: { error: 'could not find game with that key'}, status: :not_acceptable
         end
@@ -26,7 +44,8 @@ class Api::V1::GamesController < ApplicationController
     def broadcast_game
         serialized_game_data = ActiveModelSerializers::Adapter::Json.new(
         GameSerializer.new(@game)).serializable_hash
-        ActionCable.server.broadcast 'GamesChannel', serialized_game_data
-        render json: serialized_game_data
+        GamesChannel.broadcast_to(@game, serialized_game_data)
+        # render json: serialized_game_data
+        head :ok
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
       post '/join', to: 'games#join'
+      patch '/add-guess-word', to: 'games#add_guess_word'
     end
   end
   mount ActionCable.server => '/cable'
